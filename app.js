a0a1c850e692d2093cc9fa311dc46ba8c292e8ee
Praxiteles Lima <p.lima@campus.fct.unl.pt>
2024-01-21 21:43:43 +0000

    add vies and modified routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const http = require('http');
 const express = require('express');
+const path =require('path');
 
 const bodyParser = require('body-parser');
 
@@ -9,9 +10,13 @@ const app = express();
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRouter);
+app.use('/admin',adminRouter);
 app.use(shopRouter);
+
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
 
 app.listen(3000);
