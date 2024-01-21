55cb436a92a6b2bc90daa27800bd9e08f21e6a5d
Praxiteles Lima <p.lima@campus.fct.unl.pt>
2024-01-21 20:03:35 +0000

    add routes files

--- app.js
+++ app.js
@@ -6,19 +6,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{    
-    res.send('Hi from middleware');
-});
+app.use(adminRouter);
+app.use(shopRouter);
 
 app.listen(3000);
